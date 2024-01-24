b9ec2deca5242de8e55307ed5f1b3ca4f65f7acb
Chris <chris@sourcecube.de>
2024-01-24 16:36:50 +0100

    feat: add new image sets (#94)
    * feat: add new image sets
    
    * add dandelion json

--- src/FondOfKudu/Shared/ProductImageStorageConnector/ProductImageStorageConnectorConstants.php
+++ src/FondOfKudu/Shared/ProductImageStorageConnector/ProductImageStorageConnectorConstants.php
@@ -43,4 +43,199 @@ interface ProductImageStorageConnectorConstants
      * @var string
      */
     public const IMAGE_SET_SETIMAGE = 'SetImage';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_FRONT = 'img_front';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_FRONT_LEFT = 'img_front_left';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_FRONT_RIGHT = 'img_front_right';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_BACK = 'img_back';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_BACK_LEFT = 'img_back_left';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_BACK_RIGHT = 'img_back_right';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_RIGHT = 'img_right';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_LEFT = 'img_left';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_TOP = 'img_top';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_SET_COMPOSING = 'img_set_composing';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_01 = 'img_detail_01';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_02 = 'img_detail_02';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_03 = 'img_detail_03';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_04 = 'img_detail_04';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_05 = 'img_detail_05';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_06 = 'img_detail_06';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_07 = 'img_detail_07';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_08 = 'img_detail_08';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_09 = 'img_detail_09';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_DETAIL_10 = 'img_detail_10';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_01 = 'img_model_01';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_02 = 'img_model_02';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_03 = 'img_model_03';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_04 = 'img_model_04';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_05 = 'img_model_05';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_06 = 'img_model_06';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_07 = 'img_model_07';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_08 = 'img_model_08';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_09 = 'img_model_09';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_MODEL_13 = 'img_model_13';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_SOLE = 'img_sole';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_LEFT_INSIDE = 'img_left_inside';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_RIGHT_INSIDE = 'img_right_inside';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_LEFT_OUTSIDE = 'img_left_outside';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_RIGHT_OUTSIDE = 'img_right_outside';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_USP_01 = 'img_usp_01';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_USP_02 = 'img_usp_02';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_USP_03 = 'img_usp_03';
+
+    /**
+     * @var string
+     */
+    public const IMAGE_SET_SIZE_CHART = 'img_size_chart';
 }
